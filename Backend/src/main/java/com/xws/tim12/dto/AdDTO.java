d0c928d7142dd7067bcb8c5e5d070471cb6c9f4f
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-05-15 14:39:00 +0200

    Uradjen DTO za Ad, Agent, NormalUser i Comment.

---
+++ Backend/src/main/java/com/xws/tim12/dto/AdDTO.java
@@ -0,0 +1,61 @@
+package com.xws.tim12.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotEmpty;
+
+import com.xws.tim12.model.Comment;
+import com.xws.tim12.model.Vehicle;
+
+public class AdDTO {
+	
+    private Long id;
+    
+    @NotEmpty(message = "Vehicle is empty.")
+    private Vehicle vehicle;
+    private List<Comment> comments;
+
+    public AdDTO() {
+
+    }
+    
+    public AdDTO(Vehicle vehicle) {
+        this.vehicle = vehicle;
+        this.comments = new ArrayList<Comment>();
+    }
+
+    public AdDTO(Vehicle vehicle, List<Comment> comments) {
+        this.vehicle = vehicle;
+        this.comments = comments;
+    }
+    
+    public AdDTO(AdDTO ad) {
+        this.vehicle = ad.vehicle;
+        this.comments = ad.comments;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+}
