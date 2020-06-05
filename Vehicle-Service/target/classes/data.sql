aa551e445de40265ba44f4810575c9df67c5dc2a
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-06-05 21:31:24 +0200

    Mikro servis za Cart i Request.
    Uradjena je vecina funkcionalnosti za ovaj servis. Nije podesen docker.

---
+++ Vehicle-Service/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into vehicle (brand, model, fuel_type, transmission_type, vehicle_type, price, travelled_mileage, planned_mileage_to_travel, is_mileage_unlimited, has_collision_damage_waiver, number_of_seats, grade, available, vehicle_discount, insurance_price)
+values ('Mercedes', 'C klasa', 'GASOLINE', 'MANUAL', 'SEDAN', '15000', '2323', '232', 'false', 'false', '5', '5', 'true', '10', '300');
